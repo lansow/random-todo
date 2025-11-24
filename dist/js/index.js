e0fcf338522cd8edeb3ed205db72070663194ef1
lansow <lanwoyt@gmail.com>
2025-11-24 22:44:41 +0330

    first commit

---
+++ dist/js/index.js
@@ -0,0 +1,15 @@
+const random = 13241342;
+console.log(random.toString());
+function rt() {
+    return true;
+}
+function login(user, pass, randomC) {
+    if (randomC() === true) {
+        console.log("aaaaaaaaaaaaaaaaaaaaaaa");
+        return false;
+    }
+    return true;
+}
+login("jdhksad", "hsagdhjg3", rt);
+export {};
+//# sourceMappingURL=index.js.map
